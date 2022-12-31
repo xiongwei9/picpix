d183ee353f05ad7a507ecf8af0b13853c105dccb
xiongwei.zhu <zhuxiongwei@foxmail.com>
2022-12-31 11:42:22 +0800

    chore: color comment

--- lib/color.ts
+++ lib/color.ts
@@ -1,6 +1,3 @@
-// 色差计算
-// 参考文档：https://blog.csdn.net/qq_16564093/article/details/80698479
-
 interface RGB {
   R: number;
   G: number;
@@ -28,7 +25,8 @@ function newRGB(color: string): RGB {
 
 /**
  * 使用LAB颜色空间计算色差
- * https://www.compuphase.com/cmetric.htm
+ * - https://www.compuphase.com/cmetric.htm
+ * - https://blog.csdn.net/qq_16564093/article/details/80698479
  */
 function colorDistanceLAB(rgb1: RGB, rgb2: RGB): number {
   const rmean = (rgb1.R + rgb2.R) / 2;
@@ -41,9 +39,13 @@ function colorDistanceLAB(rgb1: RGB, rgb2: RGB): number {
 
 /**
  * 提取主题色
- * - Google算法：https://blog.csdn.net/rxie1/article/details/107635489、https://www.jianshu.com/p/a67a6d1b1844
+ * - Google算法：
+ *   - https://blog.csdn.net/rxie1/article/details/107635489
+ *   - https://www.jianshu.com/p/a67a6d1b1844
  * - 算法介绍：https://www.zhihu.com/question/430895335
- * - 主题色算法介绍：https://blog.csdn.net/shanglianlm/article/details/50051269、https://blog.csdn.net/qq_37340753/article/details/121140533
+ * - 主题色算法介绍：
+ *   - https://blog.csdn.net/shanglianlm/article/details/50051269
+ *   - https://blog.csdn.net/qq_37340753/article/details/121140533
  */
 function getThemeColor(rgbArr: RGB[]): RGB {
   return {
